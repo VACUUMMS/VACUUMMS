95d80f450512992441d4f8ace263bcc5b18d1ddc
Frank Willmore <frankwillmore@gmail.com>
2023-10-19 02:49:48 +0000

    finished first pass of gfg2voro, will dump povray, verts, or edges.

--- utils/gfg2voro.cc
+++ utils/gfg2voro.cc
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 
-// Hacking to interface to C linkage; since voro++ is C++, this also needs to be C++
+// Hacking to interface to C linkage; since voro++ is C++, gfg2voro also needs to be C++
 extern "C"
 {
 #include <ftw_param.h>
@@ -12,54 +12,10 @@ extern "C"
 }
 #include "voro++.hh"
 
-/*
-#include "voro++.hh"
-using namespace voro;
-
-// Set up constants for the container geometry
-const double x_min=-5,x_max=5;
-const double y_min=-5,y_max=5;
-const double z_min=0,z_max=10;
-
-// Set up the number of blocks that the container is divided into
-const int n_x=6,n_y=6,n_z=6;
-
-int main() {
-
-        // Create a container with the geometry given above, and make it
-        // non-periodic in each of the three coordinates. Allocate space for
-        // eight particles within each computational block
-//      container con(x_min,x_max,y_min,y_max,z_min,z_max,n_x,n_y,n_z,
-//                      false,false,false,8);
-        container con(x_min,x_max,y_min,y_max,z_min,z_max,n_x,n_y,n_z,
-                        true,true,true,8);
-
-        //Randomly add particles into the container
-        con.import("pack_ten_cube");
-
-        // Save the Voronoi network of all the particles to text files
-        // in gnuplot and POV-Ray formats
-        con.draw_cells_gnuplot("pack_ten_cube.gnu");
-        con.draw_cells_pov("pack_ten_cube_v.pov");
-
-        // Output the particles in POV-Ray format
-        con.draw_particles_pov("pack_ten_cube_p.pov");
-}
-
-// end of example */
-
 int main(int argc, char *argv[])
 {
-  char line[256];
-  char *xs, *ys, *zs, *ds, *es;
-  double x, y, z, d, e;
-  const char *color="White";
-  double box_x=10, box_y=10, box_z=10;
-//  int verts = 0;
-//  int edges = 0;
-  char transmit_str[256] = "";
-  double phong = 0.0;
-  char phong_str[256] = "";
+
+  // Parse command line options
 
   setCommandLineParameters(argc, argv);
   if (getFlagParam((char*)"-usage"))
@@ -69,23 +25,27 @@ int main(int argc, char *argv[])
     printf("                         -box [ n.nn n.nn n.nn ] \n");
     return 0;
   }
-
-  // Create a temp file for input to voro++
-  char temp_name[] = "/tmp/vacuumms-XXXXXX";
-  int fd = mkstemp(temp_name);
-  printf("FTW: got temp_name = >>>%s<<<\n", temp_name);
-  FILE* tempfile=fopen(temp_name, "w");
-
-  // Parse command line
+  double box_x=10, box_y=10, box_z=10;
   getVectorParam((char*)"-box", &box_x, &box_y, &box_z);
   int verts = getFlagParam((char*)"-verts"); // dump vertices
   int edges = getFlagParam((char*)"-edges"); // dump edges
   int povray = getFlagParam((char*)"-povray"); // povray format
 
-  printf("FTW: got box dims = %lf x %lf x %lf \n", box_x, box_y, box_z);
+
+  // get centers from gfg and dump to temp file for input to voro++
+
+  char temp_name[] = "/tmp/vacuumms-XXXXXX";
+  if (mkstemp(temp_name) == -1) 
+  {
+    printf("vacuumms: could not create temp file %s\n", temp_name);
+    exit(1);
+  }
+  FILE* tempfile=fopen(temp_name, "w");
 
   int particle_number=0;
-  //while (1) // loop over all lines
+  char line[256];
+  char *xs, *ys, *zs, *ds, *es;
+  double x, y, z, d, e;
   while (++particle_number) // loop over all lines
   {
     fgets(line, 256, stdin);
@@ -104,14 +64,15 @@ int main(int argc, char *argv[])
     e = strtod(es, NULL);
     
     // Grab the centers and write to the tempfile
-    // printf("%lf,%lf,%lf\n", x, y, z);
     fprintf(tempfile, "%d %lf %lf %lf\n", particle_number, x, y, z);
   }
+  
+  // clean up
   fclose(stdin);
   fclose(tempfile);
 
 
-  // Now do the voro++ stuff.
+  // Run voro++ on the temporary structure file
   
   // Set up constants for the container geometry
   const double x_min=0, x_max=box_x;
@@ -121,62 +82,65 @@ int main(int argc, char *argv[])
   // Set up the number of blocks that the container is divided into
   const int n_x=6,n_y=6,n_z=6;
 
+  // create container and add particles into the container
   voro::container con(x_min,x_max,y_min,y_max,z_min,z_max,n_x,n_y,n_z, true,true,true,8);
-  //Randomly add particles into the container
   con.import(temp_name);
 
   // Create a temp file for output from voro++
   char pov_temp_name[] = "/tmp/vacuumms-pov-XXXXXX";
-  int pov_fd = mkstemp(pov_temp_name);
-  printf("Got pov_temp_name = %s\n", pov_temp_name);
-  printf("Got pov_fd = %d\n", pov_fd);
+  if (mkstemp(pov_temp_name) == -1) 
+  {
+    printf("vacuumms: could not create temp file %s\n", pov_temp_name);
+    exit(1);
+  }
   con.draw_cells_pov(pov_temp_name);
 
 
-  // Dump the results back to stdout
+  // Format and dump the voro++ results back to stdout
 
   if (verts)
   {
-      char command[1024];
-      sprintf(command, "grep sphere < %s | sed -e \"s/sphere{<//g\" | sed -e \"s/>.*//g\" |tr \",\" \"\t\"", pov_temp_name);
+    char command[1024];
+    double x0, y0, z0;
+    sprintf(command, "grep sphere < %s", pov_temp_name);
 
-      FILE* process = popen(command, "r"); 
-      while(1)
-      {
-        fgets(line, 256, process);
-        if (feof(process)) break;
-        printf("%s", line);
-      }
-      fclose(process);
+    FILE* process = popen(command, "r"); 
+    while(fgets(line, 256, process))
+    {
+      sscanf(line, "sphere{<%lf,%lf,%lf>,r}", &x0, &y0, &z0);
+      printf("%lf\t%lf\t%lf\n", x0, y0, z0);
+    }
+    fclose(process);
   }
   else if (edges)
   {
-      char command[1024];
-      // cylinder{<3,13,13>,<3,13,3>,r}
+    char command[1024];
+    // cylinder{<3,13,13>,<3,13,3>,r}
 
-      sprintf(command, "grep cylinder < %s", pov_temp_name);
-      FILE* process = popen(command, "r"); 
-      double x0,y0,z0,x1,y1,z1;
-// need to find a way to break loop
-      while(1)
-      {
-//        fgets(line, 256, process);
-        int retval = fscanf(process, "cylinder{<%lf,%lf,%lf>,<%lf,%lf,%lf>,r}", &x0, &y0, &z0, &x1, &y1, &z1);
-printf("FTW retval is %d\n", retval);
-if (retval == 0) break;
-        if (feof(process)) break;
-        printf("got <%lf,%lf,%lf> - <%lf,%lf,%lf>!\n", x0, y0, z0, x1, y1, z1);
-      }
-      fclose(process);
-  } 
- 
+    sprintf(command, "grep cylinder < %s", pov_temp_name);
+    FILE* process = popen(command, "r"); 
+    double x0, y0, z0, x1, y1, z1;
+
+    while(fgets(line, 256, process))
+    {
+        sscanf(line, "cylinder{<%lf,%lf,%lf>,<%lf,%lf,%lf>,r}", &x0, &y0, &z0, &x1, &y1, &z1);
+        printf("%lf\t%lf\t%lf\t%lf\t%lf\t%lf\n", x0, y0, z0, x1, y1, z1);
+    }
+    fclose(process);
+  }
+  else if (povray) // just dump the raw file to stdout
+  {
+    FILE* process = fopen(pov_temp_name, "r");
+    while(fgets(line, 256, process)) printf("%s", line); 
+    fclose(process);
+  }
   
-//  FILE *popen(const char *command, const char *mode); [Option End]
 
   // clean up
-//  remove(temp_name);
-//  remove(pov_temp_name);
+
+  remove(temp_name);
+  remove(pov_temp_name);
+
 
   return 0;
 }
-
