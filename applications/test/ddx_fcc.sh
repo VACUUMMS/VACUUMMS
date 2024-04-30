2b5d9b611200f2da9da2bc67a9f933a64d4bcde8
Frank Willmore <frankwillmore@gmail.com>
2024-04-30 11:02:01 +0000

    added example tests and tests for ljx and (p)ddx

--- applications/test/ddx_fcc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "source>>"${BinaryPath}
-echo "project>>"${TestDataPath}
-
-${BinaryPath}/cmake/applications/ddx < ${TestDataPath}/applications/ddx/fcc.gfg -box 4.242640687119285 4.242640687119285 4.242640687119285 -n 10 > fcc.out
-
-diff fcc.out ${TestDataPath}/applications/test/ddx_fcc.cav
-
