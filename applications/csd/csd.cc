79ec0560988c21eaeb72b620977714735002b8d1
Frank Willmore <frankwillmore@gmail.com>
2024-12-19 16:32:13 -0800

    reworked csd.cc as example of using OO approach

---
+++ applications/csd/csd.cc
@@ -0,0 +1,35 @@
+/* csd.cc */
+/* input file should be of .cav format */
+/* output is of .hst format */
+
+#include <stdio.h>
+#include <string.h>
+
+#include <vacuumms/parameters.hh>
+#include <vacuumms/cavity.hh>
+
+int n_bins = 100;
+double resolution = .01;
+char *input_file_name;
+int histogram[1000];
+
+int main(int argc, char *argv[])
+{
+    Parameters p(argc, argv);
+  
+    p.getStringParam("input_file_name", &input_file_name);
+    p.getIntParam("n_bins", &n_bins);
+    p.getDoubleParam("resolution", &resolution);
+
+    CavityConfiguration cc(input_file_name);
+
+    for (int i=0; i<cc.getSize(); i++)
+    {
+        int which_bin = (int)(cc.recordAt(i).d / resolution);
+        histogram[which_bin]++;
+    }
+
+    for (int i=0; i<n_bins; i++) printf("%lf\t%d\n", i*resolution, histogram[i]);
+  
+    return 0;
+}
