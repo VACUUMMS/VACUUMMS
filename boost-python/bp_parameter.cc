654550c119db4168620abb6778686b9bd33e1f6e
Frank Willmore <frankwillmore@gmail.com>
2025-01-07 13:07:48 -0800

    first pass at boost-python bindings skeleton. Needs CMake code to find boost and python libs and headers

---
+++ boost-python/bp_parameter.cc
@@ -0,0 +1,16 @@
+
+#include <boost/python.hpp>
+#include <iostream>
+
+#include <vacuumms/parameters.hh>
+
+using namespace boost::python;
+
+BOOST_PYTHON_MODULE(vacuumms) 
+{
+
+    class_<Parameters>("Parameters", init<int, char**>())
+        .def("getIntParam", &Parameters::getIntParam) ;
+
+}
+
---
+++ boost-python/env.sh
@@ -0,0 +1,12 @@
+export CPATH=/home1/fwillmor/miniconda3/include/python3.12
+
+# Get the python side of boost
+# conda install boost
+conda activate
+
+# Get the C++ side from libraries in the module
+ml boost
+
+# Get the python and boost_python libs in the path
+export LIBRARY_PATH=/home1/fwillmor/miniconda3/lib:${LIBRARY_PATH} 
+export LD_LIBRARY_PATH=/home1/fwillmor/miniconda3/lib:${LD_LIBRARY_PATH} 
